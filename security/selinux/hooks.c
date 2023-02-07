1379b5a861c5ade5e8adb3b0a8b4fa1a1ef9953e
F-19-F <58457605+F-19-F@users.noreply.github.com>
2023-02-07 17:14:27 +0800

    selinux: Allow init exec ksud under nosuid
    [TogoFire]: Guard KernelSU changes.
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I7cd2a53ccf59e99ec17305d905dbf2039e161fe0

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2303,18 +2303,24 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 	u32 av;
 	u32 seclen;
 
+#ifdef CONFIG_KSU
+		static u32 ksu_sid;
+		char *secdata;
+		int error;
+		u32 seclen;
+#endif
+
 	if (!nnp && !nosuid)
 		return 0; /* neither NNP nor nosuid */
 
 	if (new_tsec->sid == old_tsec->sid)
 		return 0; /* No change in credentials */
 
+#ifdef CONFIG_KSU
 	if(!ksu_sid){
 		security_secctx_to_secid("u:r:su:s0", strlen("u:r:su:s0"), &ksu_sid);
 	}
-
 	error = security_secid_to_secctx(old_tsec->sid, &secdata, &seclen);
-
 	if (!error) {
 		rc = strcmp("u:r:init:s0",secdata);
 		security_release_secctx(secdata, seclen);
@@ -2322,6 +2328,7 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			return 0;
 		}
 	}
+#endif
 
 	/*
 	 * If the policy enables the nnp_nosuid_transition policy capability,
