2807d2cae34ac39b80e9b2f08d4104e8377efcc8
Shoiya Akari <barryofc11@gmail.com>
2025-06-03 05:27:49 +0000

    Revert "techpack/display: msm: Add idle state sysfs node"
    This reverts commit 522f11a5bd901267315acc7d031b8f6f28a6c0ad.

--- techpack/display/msm/msm_drv.h
+++ techpack/display/msm/msm_drv.h
@@ -868,15 +868,6 @@ struct msm_drm_thread {
 	struct kthread_worker worker;
 };
 
-struct msm_idle {
-	u32 timeout_ms;
-	u32 encoder_mask;
-	u32 active_mask;
-
-	spinlock_t lock;
-	struct delayed_work work;
-};
-
 struct msm_drm_private {
 
 	struct drm_device *dev;
@@ -990,9 +981,7 @@ struct msm_drm_private {
 
 	struct mutex vm_client_lock;
 	struct list_head vm_client_list;
-
-	struct msm_idle idle;
-
+	struct notifier_block msm_drv_notifier;
 };
 
 /* get struct msm_kms * from drm_device * */
@@ -1270,7 +1259,6 @@ static inline void __exit msm_mdp_unregister(void)
 }
 #endif /* CONFIG_DRM_MSM_MDP5 */
 
-void msm_idle_set_state(struct drm_encoder *encoder, bool active);
 #ifdef CONFIG_DEBUG_FS
 void msm_gem_describe(struct drm_gem_object *obj, struct seq_file *m);
 void msm_gem_describe_objects(struct list_head *list, struct seq_file *m);
