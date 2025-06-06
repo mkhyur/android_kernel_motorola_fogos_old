8599ca9790fae3fd7922e916b9233e6b8f1b4630
Shoiya Akari <barryofc11@gmail.com>
2025-06-06 21:22:20 +0000

    Revert "net/include: Update BBR to version BBR3"
    This reverts commit 78fd51ad7db435be9caaf55f75ad5e431a8e7c33.

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2078,8 +2078,7 @@ struct net_device {
 	const struct rtnl_link_ops *rtnl_link_ops;
 
 	/* for setting kernel sock attribute on TCP connection setup */
-#define GSO_LEGACY_MAX_SIZE	65536u
-#define GSO_MAX_SIZE		UINT_MAX
+#define GSO_MAX_SIZE		65536
 	unsigned int		gso_max_size;
 #define GSO_MAX_SEGS		65535
 	u16			gso_max_segs;
